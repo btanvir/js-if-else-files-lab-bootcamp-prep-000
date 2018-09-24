292677c9e2e46895dd5cc77313f06e896c14a39e
Brighton Tanvir <brightontanvir@gmail.com>
2018-09-24 01:23:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var height = 70
 // Don't worry about this
 module.exports = { name, height
 }
-// if (name === "Susan") {
-//     console.log("The name is correct")
-// } else {
-//     console.log("Expected the name to be Susan")
-// }
-// if (height === 70) {
-//     console.log("The height is correct")
-// } else {
-//     console.log("Expected: 70, Received: "+height)
-// }
+ if (name === "Susan") {
+   console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+ }
+if (height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+height)
+}
